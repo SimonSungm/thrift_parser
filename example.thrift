e00d6105838da258f08f28c001a1ab642200e1a1
SimonSungm <757651634@qq.com>
2025-04-15 20:03:08 +0800

    add comment

--- example.thrift
+++ example.thrift
@@ -12,6 +12,7 @@ struct User {
     1: i32 id = 0,
     2: string name = "unknown", #这是一条注释
     3: Status status = Status.ACTIVE; # 这是一条注释
+    //注释
     4: list<i32> scores,
     5: set<string> tags,
     6: map<string, i32> attributes
